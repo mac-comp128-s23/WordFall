af4e09f54678abbd6ccfc9fd901eae6ede810fa7
Felix Graff <felixgraff9973@gmail.com>
2023-03-29 16:10:01 -0500

    Added some technical files

---
+++ src/QuadruplyLinkedList.java
@@ -0,0 +1,20 @@
+import edu.macalester.graphics.*;
+import edu.macalester.graphics.Point;
+import edu.macalester.graphics.events.MouseButtonEvent;
+import edu.macalester.graphics.events.MouseButtonEventHandler;
+import edu.macalester.graphics.events.MouseMotionEvent;
+import edu.macalester.graphics.events.MouseMotionEventHandler;
+import edu.macalester.graphics.ui.Button;
+import edu.macalester.graphics.ui.TextField;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.EventObject;
+import java.util.function.Consumer;
+
+import org.w3c.dom.events.Event;
+
+/**Class for the individual nodes
+ * S
+ */
