4b2e06c7f015e392ad308bead6655a272a28b0dd
Felix Graff <felixgraff9973@gmail.com>
2023-04-06 15:32:21 -0500

    Updated readFile to return a Map
    readFile now returns a Map<Integer, ArrayList<String>>, where the Integer is the word length and the ArrayList<String> contains all of the words that have this length.

--- src/QuadruplyLinkedGrid.java
+++ src/QuadruplyLinkedGrid.java
@@ -1,19 +1,24 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.io.File;
 import java.util.Scanner;
+import java.util.Map;
 
 public class QuadruplyLinkedGrid {
     private final int width = 5;
     private final int height = 8;
 
     private ArrayList<ArrayList<QuadruplyLinkedNode<String>>> grid;
-    private ArrayList<String> dic;
+    private Map<Integer, ArrayList<String>> dictionary;
 
-    /*Constructor for QuadruplyLinkedNode grid. First nested loop instantiates each ArrayList within each Arraylist.
+    /*
+     * Constructor for QuadruplyLinkedNode grid. First nested loop instantiates each ArrayList within each Arraylist.
      * Second nested loop links all the nodes up.
      */
     public QuadruplyLinkedGrid() {
+        dictionary = readFile("src/wordlist.txt");
         grid = new ArrayList<ArrayList<QuadruplyLinkedNode<String>>>();
+
         for(int y = 0; y < height; y++) {
             ArrayList<QuadruplyLinkedNode<String>> row = new ArrayList<QuadruplyLinkedNode<String>>();
             for(int x = 0; x < width; x++) {
@@ -40,19 +45,25 @@ public class QuadruplyLinkedGrid {
                 //System.out.print(node);
             }
             //System.out.println();
-        }       
-        dic = readFile("src/wordlist.txt");
+        }
     }
-        //Will need a big method that checks if a word was formed in each side, making sure its checking for larger words first.
-    public void wordChecker(QuadruplyLinkedNode<String> current){
+    
+    /**
+     * Checks for any words 
+     * @param current
+     */
+    public ArrayList<String> wordChecker(QuadruplyLinkedNode<String> current){
+        ArrayList<String> wordsFound = new ArrayList<String>();
         QuadruplyLinkedNode<String>[] takenOut;
         StringBuilder word = new StringBuilder("");
         int row;
+        
         for(int i = 0; i < grid.size(); i++){
             if (grid.get(i).contains(current)){
-                row 
+                
             }
         }
+
         for (int i = width; i>= 2; i--){
             for(int j = 0; j+i <= width; j++){
                 for(int k = 0; k< i; k++){
@@ -60,6 +71,8 @@ public class QuadruplyLinkedGrid {
                 }
             }
         }
+
+        return wordsFound;
     }
 
     /**
@@ -68,21 +81,30 @@ public class QuadruplyLinkedGrid {
      * @param fileName
      * @return
      */
-    public ArrayList<String> readFile(String fileName) {
-        ArrayList<String> wordList = new ArrayList<String>();
+    public Map<Integer, ArrayList<String>> readFile(String fileName) {
+        Map<Integer, ArrayList<String>> wordMap = new HashMap<Integer, ArrayList<String>>();
         try {
             File myFile = new File(fileName);
             Scanner fileScanner = new Scanner(myFile);
+
+            // For each word in the file (since they are separated by new lines):
             while(fileScanner.hasNextLine()) {
-                String data = fileScanner.nextLine().toLowerCase();
-                wordList.add(data);
+                String word = fileScanner.nextLine().toLowerCase();
+                int wordLength = word.length();
+                ArrayList<String> updatedList = wordMap.get(wordLength);
+                updatedList.add(word);
+
+                // Adding words to the map based on their length
+                wordMap.put(wordLength, updatedList);
             }
+
             fileScanner.close();
         } catch(Exception e) {
             System.out.println("File not found!");
             e.printStackTrace();
         }
-        return wordList;
+
+        return wordMap;
     }
 
     public static void main(String[] args) {
