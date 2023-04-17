d89689ebea32926a94e2bdbaef2680d81794e863
Felix Graff <felixgraff9973@gmail.com>
2023-04-17 16:32:40 -0500

    Started manual time detection
    It just kind of... doesn't work as of now though. :P

--- src/Game.java
+++ src/Game.java
@@ -1,7 +1,7 @@
 import edu.macalester.graphics.*;
 
 public class Game {
-    private final String alphabet = "abcdefghijklmnopqrstuvwxyz";
+    private final String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
     private final int CANVAS_WIDTH = 800;
     private final int CANVAS_HEIGHT = 800;
@@ -9,7 +9,8 @@ public class Game {
     private final int GAME_WIDTH = 375;
     private final int GAME_HEIGHT = 600;
 
-    private int letterStepTime = 1000; //milliseconds
+    private int stepInterval; // in milliseconds
+    private long lastStepTime;
 
     private int fallingLetterRow;
     private int fallingLetterCol;
@@ -19,30 +20,36 @@ public class Game {
     private boolean gameIsRunning;
     private boolean letterHasLanded;
 
-    public void moveDown() {
-        if(gameIsRunning) {
-            //TODO: Check for game end
-            if(letterHasLanded) {
-                int random = (int)(Math.random() * 26);
-                String letter = alphabet.substring(random, random + 1);
-                grid.setNode(0, 2, letter);
-
-                fallingLetterRow = 0;
-                fallingLetterCol = 2;
-
-                letterHasLanded = false;
-            } else {
-                QNode<String> fallingNode = grid.getNode(fallingLetterRow, fallingLetterCol);
-                if(!fallingNode.setLower(fallingNode.getValue())) {
-                    letterHasLanded = true;
-                } else {
-                    fallingLetterRow++;
-                }
-            }
-
-            canvas.pause(letterStepTime);
-            drawGrid();
+    public void moveDown(long lastStepTime) {
+        // Wait until the step interval has passed and make sure the game is still running.
+        if(System.currentTimeMillis() - lastStepTime < stepInterval || !gameIsRunning) {
+            return;
         }
+
+        lastStepTime = System.currentTimeMillis();
+        System.out.println(lastStepTime);
+
+        //TODO: Check for game end
+        if(letterHasLanded) {
+            int random = (int)(Math.random() * 26);
+            String letter = alphabet.substring(random, random + 1);
+            grid.setNode(0, 2, letter);
+
+            fallingLetterRow = 0;
+            fallingLetterCol = 2;
+
+            letterHasLanded = false;
+        } else {
+            QNode<String> fallingNode = grid.getNode(fallingLetterRow, fallingLetterCol);
+            if(!fallingNode.setLower(fallingNode.getValue())) {
+                letterHasLanded = true;
+            } else {
+                fallingLetterRow++;
+            }
+        }
+
+        // Updates the grid to show the new letter positions
+        drawGrid();
     }
 
     /**
@@ -52,12 +59,15 @@ public class Game {
     public Game() {
         letterHasLanded = true;
         gameIsRunning = true;
+        stepInterval = 1000;
+        lastStepTime = System.currentTimeMillis();
 
         canvas = new CanvasWindow("Word Capture", CANVAS_WIDTH, CANVAS_HEIGHT);
         grid = new QGrid();
         drawGrid();
 
-        canvas.animate(() -> moveDown());
+        // The lambda functions that run the game:
+        canvas.animate(() -> moveDown(lastStepTime));
 
         canvas.onKeyDown(e -> {
             moveFallingLetter(e.getKey().toString());
@@ -69,7 +79,11 @@ public class Game {
      * @param key The name of the key ("LEFT_ARROW" or "RIGHT_ARROW")
      */
     private void moveFallingLetter(String key) {
-        //TODO: Add code to make the falling letter move left or right based on which arrow keys the user presses.
+        // The letter can't be moved if it's already landed.
+        if(letterHasLanded) {
+            return;
+        }
+
         QNode<String> fallingNode = grid.getNode(fallingLetterRow, fallingLetterCol);
         if(key.equals("LEFT_ARROW")) {
             if(fallingNode.setLeft(fallingNode.getValue())) {
@@ -107,7 +121,7 @@ public class Game {
                 grid.getNode(row, col).setGraphics(group);
                 group.add(rect);
 
-                GraphicsText text = new GraphicsText(grid.getNode(row, col).getValue(), 0, 0); //grid.getNode(row, col).getValue()
+                GraphicsText text = new GraphicsText(grid.getNode(row, col).getValue(), 0, 0);
                 text.setCenter(sideLength / 2, sideLength / 2);
                 text.setFontSize(20);
                 group.add(text);
